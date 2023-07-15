5f73baab33759e75b699337d04c359f4e0d3e103
Mathis De Witte <mathisdw@icloud.com>
2023-07-15 18:17:10 +0200

    clauses for stmt

--- src/base_language/StatementElement.java
+++ src/base_language/StatementElement.java
@@ -2,20 +2,28 @@ package base_language;
 
 import meta_lang.ParseResult;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
 
 public class StatementElement implements AbstractSyntaxElement {
     AbstractSyntaxElement stmt;
+
+    public Set<Supplier<AbstractSyntaxElement>> clauses = new HashSet<>(Arrays.asList(
+            ReturnStatementElement::new,
+            PrintStatementElement::new
+    ));
     @Override
     public ParseResult parse(String input) {
         ParseResult res;
 
-        if ((res = new ReturnStatementElement().parse(input)).parsedResult().isPresent()) {
-            stmt = res.parsedResult().get();
-            return new ParseResult(Optional.of(this), res.leftOverString());
-        } else if ((res = new PrintStatementElement().parse(input)).parsedResult().isPresent()) {
-            stmt = res.parsedResult().get();
-            return new ParseResult(Optional.of(this), res.leftOverString());
+        for (Supplier<AbstractSyntaxElement> clause : clauses) {
+            if ((res = clause.get().parse(input)).parsedResult().isPresent()) {
+                stmt = res.parsedResult().get();
+                return new ParseResult(Optional.of(this), res.leftOverString());
+            }
         }
 
         return new ParseResult(Optional.empty(), input);
